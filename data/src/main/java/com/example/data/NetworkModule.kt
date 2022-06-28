f02585737f3749c2611336e7bd5a28296089b0ff
alaeddine.oueslati <alaeddine.oueslati+bnpp@octo.com>
2022-06-28 22:32:34 +0200

    update network mobile

--- data/src/main/java/com/example/data/NetworkModule.kt
+++ data/src/main/java/com/example/data/NetworkModule.kt
@@ -6,15 +6,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 open class NetworkModule {
 
     companion object {
-        private const val BASE_URL = "https://pro.openweathermap.org/data/2.5/"
+        private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     }
 
-    fun provideRetrofit(url : String = BASE_URL): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(url: String = BASE_URL): Retrofit = Retrofit.Builder()
         .baseUrl(url)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-
-    fun provideWeatherService(): WeatherService{
-        return provideRetrofit().create(WeatherService::class.java)
-    }
 }
