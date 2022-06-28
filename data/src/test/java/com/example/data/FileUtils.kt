0739ef55096747aeabfa736b62da424ecf2c85fa
Alaeddine23 <oueslati.alaeddine23@gmail.com>
2022-06-28 13:33:20 +0200

    update fileutils

--- data/src/test/java/com/example/data/FileUtils.kt
+++ data/src/test/java/com/example/data/FileUtils.kt
@@ -2,6 +2,7 @@ package com.example.data
 
 import org.apache.commons.io.IOUtils
 import java.io.IOException
+import java.io.InputStreamReader
 
 class FileUtils {
     companion object {
@@ -13,5 +14,13 @@ class FileUtils {
         } catch (e: IOException) {
             null
         }
+
+        fun readJson(fileName: String): String {
+            val reader = InputStreamReader(CLASS_LOADER.getResourceAsStream(fileName))
+            val content = reader.readText()
+            reader.close()
+            return content
+        }
+
     }
 }
