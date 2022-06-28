f745b54553e56c75de26700f95c7252fa70597a9
alaeddine.oueslati <alaeddine.oueslati+bnpp@octo.com>
2022-06-28 22:32:34 +0200

    update network mobile

--- data/src/main/java/com/example/data/WeatherNetworkRepository.kt
+++ data/src/main/java/com/example/data/WeatherNetworkRepository.kt
@@ -4,6 +4,11 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 class WeatherNetworkRepository(private val retrofit: Retrofit) {
+    companion object {
+        private const val API_KEY = "ec3b4790643f7d21ceb6cc1d9e2d8a8e"
+        private const val UNIT = "metric"
+    }
+
     suspend fun loadCityForecasts(cityName: String, apiKey: String, unit: String): Response<JsonCityWeeklyForecast>? {
         val service = retrofit.create(WeatherService::class.java)
         val response = service.getJsonCityWeeklyForecast(cityName, apiKey, unit)
